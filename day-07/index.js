6b228e956a1aee0a82c28e8f93b53226d5ae6085
Andrew Rutherford <2681131+Sidorakh@users.noreply.github.com>
2024-12-07 23:33:32 +1100

    uh whoops

--- day-07/index.js
+++ day-07/index.js
@@ -1,39 +1,15 @@
+
+
 const fs = require('fs');
 const path = require('path');
 const input = fs.readFileSync(path.join(__dirname,'./input.txt'),'utf8');
 
-
-
-function* generate_lists(length, chars='+*') {
-    let word = chars[0].repeat(length);
-    yield word;
-    for (let i=0;i<chars.length**length;i++) {
-        for (let j=0;j<length;j++) {
-            if (!(i % (chars.length ** j))) {
-                const idx = chars.indexOf(word[j])+1;
-                const char = chars[(idx) < chars.length ? idx : 0];
-                word[j] = char;
-            }
-        }
-        yield word;
-    }
-}
-
-function* charCombinations (chars, minLength, maxLength) {
-    chars = typeof chars === 'string' ? chars : '';
-    minLength = parseInt(minLength) || 0;
-    maxLength = Math.max(parseInt(maxLength) || 0, minLength);
-
+function* generate_combinations(chars, length) {
     //Generate for each word length
-    for (i = minLength; i <= maxLength; i++) {
-
-        //Generate the first word for the password length by the repetition of first character.
+    for (i = 0; i <= length; i++) {
         word = (chars[0] || '').repeat(i);
         yield word;
 
-        //Generate other possible combinations for the word
-        //Total combinations will be chars.length raised to power of word.length
-        //Make iteration for all possible combinations
         for (j = 1; j < Math.pow(chars.length, i); j++) {
 
             //Make iteration for all indices of the word
@@ -41,16 +17,13 @@ function* charCombinations (chars, minLength, maxLength) {
 
                 //check if the current index char need to be flipped to the next char.
                     if(!(j % Math.pow(chars.length, k))) {
-                    
-                    // Flip the current index char to the next.
+                    // Flip the current index char to the next
                     let charIndex = chars.indexOf(word[k]) + 1;
                     char = chars[charIndex < chars.length ? charIndex : 0];
                     word = word.substr(0, k) + char + word.substr(k + char.length);
                 }
             }
-
-            //Re-oder not neccesary but it makes the words are yeilded alphabetically on ascending order.
-            yield word.split('').reverse().join('');
+            yield word;
         }
     }
 }
@@ -60,7 +33,7 @@ module.exports.part_1 = async()=>{
     let total = 0;
     for (const statement of statements) {
         const len = statement.components.length-1;
-        const combinations = charCombinations('+*',len,len);
+        const combinations = generate_combinations('+*',len,len);
         let combo = null;
         while (combo = combinations.next()) {
             //console.log(combo.value);
@@ -90,7 +63,7 @@ module.exports.part_2 = async()=>{
     let total = 0;
     for (const statement of statements) {
         const len = statement.components.length-1;
-        const combinations = charCombinations('+*|',len,len);
+        const combinations = generate_combinations('+*|',len,len);
         let combo = null;
         while (combo = combinations.next()) {
             //console.log(combo.value);
